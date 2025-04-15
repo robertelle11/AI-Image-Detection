1b1d9d10b5356e5cbde6e5dd398a6702b930598c
NPontzer <thunderflash12@live.com>
2025-04-15 12:35:42 -0400

    added from import logic

--- pages/page2.py
+++ pages/page2.py
@@ -1,11 +1,18 @@
 import streamlit as st
 import time
 import numpy as np
+import pandas as pd
+import pydeck as pdk
+import requests
+import json
+import os
 
-st.set_page_config(page_title="Picture", page_icon="📈")
+st.set_page_config(page_title="Project Content Page", page_icon="🌍")
 
-st.markdown("# Plotting Demo")
-st.sidebar.header("Plotting Demo")
+st.markdown("Project Content Page")
+st.title("AI-Powered Image Geolocation")
+st.sidebar.header("Project Content Page")
+st.sidebar.success("Project Content Page")
 st.write(
     """This demo illustrates a combination of plotting and animation with
 Streamlit. We're generating a bunch of random numbers in a loop for around
