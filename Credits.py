61dbd6e74bd403bd00fee8a6721ea16609fc602f
Thegreenmoray <112501898+Thegreenmoray@users.noreply.github.com>
2025-04-15 12:34:52 -0400

    Credits

---
+++ Credits.py
@@ -0,0 +1,10 @@
+
+st.set_page_config(page_title="Credits", page_icon="")
+
+st.markdown("# Credits")
+st.sidebar.header("Credits")
+
+
+st.write(
+    """Cassie Allard, Alan Shen, Justin DePorter, Michael Berman, Robert Elle, Michael Szpotek, Philip Przekora, Matthew Robertson, Noah Pontzer"""
+)
